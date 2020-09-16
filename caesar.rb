0d93f8c9bca86ee104157d5e047ea873fdda3d49
isaacrez <isaacrez20@gmail.com>
2020-09-16 12:51:09 -0400

    Update to match testing syntax

--- caesar.rb
+++ caesar.rb
@@ -1,51 +1,54 @@
-def caesar_cipher(string, shift)
-    cipher_array = []
+class CaesarCipher
+  def encode(string, shift)
+      cipher_array = []
 
-    string.each_codepoint do |code|
-        shifted_char = process_code(code, shift).chr
-        cipher_array.push(shifted_char)
-    end
-    cipher_array.join()
-end
+      string.each_codepoint do |code|
+          shifted_char = process_code(code, shift).chr
+          cipher_array.push(shifted_char)
+      end
+      cipher_array.join()
+  end
 
-def process_code(code, shift)
-    form = get_case_by_code(code)
-    unless form == "n/a"
-        code += shift
-        code = force_valid_shift(code, form)
-    end
-    return code
-end
+  private
+  def process_code(code, shift)
+      form = get_case_by_code(code)
+      unless form == "n/a"
+          code += shift
+          code = force_valid_shift(code, form)
+      end
+      return code
+  end
 
-def force_valid_shift(code, form)
-    case form
-    when 'lower'
-        offset = get_code('a')
-    when 'upper'
-        offset = get_code('A')
-    end
+  def force_valid_shift(code, form)
+      case form
+      when 'lower'
+          offset = get_code('a')
+      when 'upper'
+          offset = get_code('A')
+      end
 
-    (code - offset) % 26 + offset
-end
+      (code - offset) % 26 + offset
+  end
 
-def get_case_by_code(code)
-    if is_lower_by_code(code)
-        return 'lower'
-    elsif is_upper_by_code(code)
-        return 'upper'
-    else
-        return 'n/a'
-    end
-end
+  def get_case_by_code(code)
+      if is_lower_by_code(code)
+          return 'lower'
+      elsif is_upper_by_code(code)
+          return 'upper'
+      else
+          return 'n/a'
+      end
+  end
 
-def is_lower_by_code(code)
-    get_code('a') <= code && code <= get_code('z')
-end
+  def is_lower_by_code(code)
+      get_code('a') <= code && code <= get_code('z')
+  end
 
-def is_upper_by_code(code)
-    get_code('A') <= code && code <= get_code('Z')
-end
+  def is_upper_by_code(code)
+      get_code('A') <= code && code <= get_code('Z')
+  end
 
-def get_code(char)
-    char.each_codepoint.first
+  def get_code(char)
+      char.each_codepoint.first
+  end
 end
